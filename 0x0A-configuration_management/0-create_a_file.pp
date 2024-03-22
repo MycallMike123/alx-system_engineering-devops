6c21f0295d4d375b9d4acbeaa3212ba93c337d0d
Michael Mwangi <michaelmichire2016@gmail.com>
2024-03-22 06:39:11 -0700

    Creates a file named school in /tmp with specified permissions, owner, group, and content

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,14 +1,9 @@
 # Creates a class using Puppet
 
-class school_file {
-
-	file { '/tmp/school':
-		ensure  => file,            # Ensure it's a file
-		mode    => '0744',          # Set file permissions
-		owner   => 'www-data',      # Set owner
-		group   => 'www-data',      # Set group
-		content => "I love Puppet",  # Set file content
-	}
+file { '/tmp/school':
+	ensure  => file,            # Ensure it's a file
+	mode    => '0744',          # Set file permissions
+	owner   => 'www-data',      # Set owner
+	group   => 'www-data',      # Set group
+	content => "I love Puppet",  # Set file content
 }
-
-include school_file
