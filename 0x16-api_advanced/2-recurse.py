55f7603ff6606c5d2675418104f32f3fcb4bf6b8
Michael Mwangi <michaelmichire2016@gmail.com>
2024-05-07 06:28:57 -0700

    A recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit. If no results are found for the given subreddit, the function should return None

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Using reddit's API
+"""
+
+import requests
+after = None
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    Recursively queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+        hot_list (list): A list to store the titles of hot articles. Defaults to an empty list.
+
+    Returns:
+        list: A list containing the titles of all hot articles for the given subreddit, or None if no results are found.
+        """
+        global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
+
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
+    else:
+        return (None)
