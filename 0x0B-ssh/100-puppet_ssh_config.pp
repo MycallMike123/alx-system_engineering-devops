11152d5289ba5a6e5617d658a05f6b2b3fd806ec
Michael Mwangi <michaelmichire2016@gmail.com>
2025-06-29 13:13:06 +0300

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,20 @@
-file_line { 'Set IdentityFile':
-  path  => '/home/mycall/.ssh/config',
-  line  => '    IdentityFile ~/.ssh/school',
-  match => '^\\s*IdentityFile\\s+',
+# making changes to the default ssh config file using Puppet
+# connect to a server without typing a password
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-file_line { 'Disable PasswordAuthentication':
-  path  => '/home/mycall/.ssh/config',
-  line  => '    PasswordAuthentication no',
-  match => '^\\s*PasswordAuthentication\\s+',
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
